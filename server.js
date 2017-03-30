c253e784f7c6f08a74980035741ce59691a74ee9
Håkon Harnes <sosser@skymiastudios.com>
2017-03-30 17:36:41 +0200

    Setup hello world
    Setup hello world response from server

--- server.js
+++ server.js
@@ -6,10 +6,16 @@ const mongoClient = require('mongodb').MongoClient
 
 const app = express();
 
+console.log("Connecting to MongoDB...");
 mongoClient.connect(config.db_connection_string, (err, database) => {
   if (err) return console.log(err)
+  console.log("Connected to DB, starting server...");
   db = database
   app.listen(3000, () => {
     console.log('listening on 3000')
+  })
+
+  app.get("/", (req,res) => {
+      res.send("Hello world")
   })
 })
