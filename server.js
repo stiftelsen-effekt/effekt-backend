7218fdd03c1a9c1bdd6a3db3820927754101d016
Håkon Harnes <sosser@skymiastudios.com>
2017-03-31 18:38:17 +0200

    Switch from MongoDB to mongoose
    Switch from MongoDB to mongoos, and add initial model

--- server.js
+++ server.js
@@ -1,21 +1,40 @@
+console.log("Loading config")
+
 const config = require('./config.js')
+
+console.log("Loading dependencies")
 
 const express = require('express')
 const bodyParser = require('body-parser')
-const mongoClient = require('mongodb').MongoClient
+const mongoose = require('mongoose')
+const validator = require('validator')
 
 const app = express();
 
-console.log("Connecting to MongoDB...");
-mongoClient.connect(config.db_connection_string, (err, database) => {
-  if (err) return console.log(err)
-  console.log("Connected to DB, starting server...");
-  db = database
-  app.listen(3000, () => {
-    console.log('listening on 3000')
-  })
+console.log("Connecting to DB")
+mongoose.connect(config.db_connection_string)
 
-  app.get("/", (req,res) => {
-      res.send("Hello world")
-  })
+//Models
+var User = mongoose.model('User', { 
+  mail: {
+    type: String,
+    uniqe: true,
+    required: true,
+    validate: validator.isEmail
+  },
+  KID: {
+    type: Number,
+    required: true,
+    min: 12,
+    max: 12
+  }
+})
+
+//Server
+app.listen(3000, () => {
+  console.log('listening on 3000')
+})
+
+app.get("/", (req,res) => {
+    res.send("Hello world")
 })
