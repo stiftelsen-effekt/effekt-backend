b4b4a5ff7031c976a77a6f6ea628459516720293
Håkon Harnes <sosser@skymiastudios.com>
2017-05-03 15:17:12 +0200

    Add getDonationByUser

--- models/donation.js
+++ models/donation.js
@@ -45,8 +45,75 @@ DonationSchema.statics.registerDonationsFromOCR = (OCRdonations, cb) => {
     }).exec((err, donations) => {
         if (err) return cb(err)
         cb(null, donations)
-    });
-};
+    })
+}
+
+DonationSchema.statics.getDonationByUser = (userID, cb) => {
+  model.find({
+    ownerID: userID
+  }).exec((err, donations) => {
+    if (err) return cb(err)
+    cb(null, donations)
+  })
+}
+
+DonationSchema.statics.getAggregatedDonationsByUser = (userID, fromDate, toDate, cb) => {
+  Donation.aggregate([
+    {
+      $match: {
+        ownerID: userID,
+        registered: { 
+          $gte: fromDate,
+          $lt: toDate
+        }
+      }
+    }
+  ], (err, donations) => {
+    
+  })
+}
+
+DonationSchema.statics.getTotalAggregatedDonations = (from, to, cb) => {
+  model.aggregate([
+    {
+      $match: {
+        verified: true,
+        registered: { 
+          $gte: from,
+          $lt: to
+        }
+      }
+    },
+    {
+      $project: {
+        split: 1,
+        amount: 1
+      }
+    },
+    {
+      $unwind: "$split"
+    },
+    {
+      $project: {
+        result: {
+          $multiply: ["$amount", "$split.share", 0.01] //0.01 to change from % to to 0-1
+        },
+        organizationID: "$split.organizationID"
+      }
+    },
+    {
+      $group: {
+        _id: "$organizationID",
+        sum: {
+          $sum: "$result"
+        }
+      }
+    }
+  ], (err, result) => {
+    if (err) return cb(err)
+    return cb(null, result)
+  })
+}
 
 const model = mongoose.model('Donation', DonationSchema, 'donations')
 
