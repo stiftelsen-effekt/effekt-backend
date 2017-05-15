4623c3957c173b5e2e8c28b10878ab626671f2de
Håkon Harnes <sosser@skymiastudios.com>
2017-05-15 10:03:24 +0200

    Setup for azure deploy
    Setup for azure deploy

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
     db_connection_string: process.env.db_connection_string, //Insert mongoDB connection string
-    mailgun_api_key: process.env.mailgun_api_key,
-    port: process.env.port
+    mailgun_api_key: process.env.mailgun_api_key, //Insert mailgun API key
+    port: process.env.PORT || 3000
 }
