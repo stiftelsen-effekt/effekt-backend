ad02f07815b616a927ca7d1b3db645f047925a59
Håkon Harnes <account@harnes.me>
2017-08-11 13:59:24 +0200

    Update rounding module for precision sum and start adding tests for percent -> absolute

--- custom_modules/rounding.js
+++ custom_modules/rounding.js
@@ -1,3 +1,5 @@
+const decimal = require('big-decimal')
+
 module.exports = {
     toPercent: function(input, decimals) {
         let total = input.reduce((acc, elem) => acc += elem, 0)
@@ -8,10 +10,21 @@ module.exports = {
             if (typeof decimals !== "undefined") result[i] = Math.round(result[i] * Math.pow(10, decimals)) / Math.pow(10, decimals)
         }
 
-        var remainder = 100 - result.reduce((acc, elem) => acc + elem, 0)
+        let houndred = new decimal('100')
+        let current = new decimal(this.sumWithPrecision(result).toString())
+        var remainder = houndred.subtract(current)
 
-        if (remainder != 0) result[result.length-1] += remainder
+        if (remainder != 0) result[result.length-1] = parseFloat(remainder.add(new decimal(result[result.length-1].toString())).toString())
 
         return result
+    },
+
+    sumWithPrecision: function(input) {
+        let total = new decimal('0')
+        for (let i = 0; i < input.length; i++) {
+            total = total.add(new decimal(input[i].toString()))
+        }
+
+        return parseFloat(total.toString())
     }
 }
--- test/rounding.js
+++ test/rounding.js
@@ -3,42 +3,95 @@ const expect = (chai.expect)
 
 const round = require('../custom_modules/rounding.js')
 
-const testCases = [
-    [100,100,100,100,100],
-    [213,12,98,77,201]
-]
+describe('sumWithPrecision', function() {
+    const testCases = [
+        [50, 50],
+        [10, 12, 14, 16, 18, 20, 10],
+        [0.002, 99.998],
+        [33.33333333, 33.33333333, 33.33333334],
+        [21.2, 12.4, 8.7, 9.326, 8.1, 12.7863144, 8, 10, 9.4876856],
+        [100],
+        [0,0,0,100]
+    ]
+
+    it('should be a functoin', function() {
+        expect(round.sumWithPrecision).to.be.a('function')
+    })
+
+    it('should return a number', function() {
+        expect(round.sumWithPrecision([50,50])).to.be.a('number')
+    })
+
+    it('should return 100 when supplied with array that totals 100', function() {
+        for(let i = 0; i < testCases.length; i++) {
+            expect(round.sumWithPrecision(testCases[i])).to.equal(100)
+        }
+    })
+})
 
 describe('round', function() {
-    it('toPercent should be a function', function() {
-        expect(round.toPercent).to.be.a('function')
-    })
+    describe('toPercent' , function() {
+        const testCases = [
+            [100,100,100,100,100],
+            [213,12,98,77,201],
+            [2,1],
+            [1034, 276, 3, 96, 4, 546, 34, 893, 1000],
+            [333, 333, 334]
+        ]
 
-    it('toPercent should return an array', function() {
-        expect(round.toPercent(testCases[0])).to.be.an('array')
-    })
+        it('should be a function', function() {
+            expect(round.toPercent).to.be.a('function')
+        })
 
-    it('toPercent should return an array of equal length as input', function() {
-        expect(round.toPercent(testCases[0])).to.be.length(testCases[0].length)
-    })
+        it('should return an array', function() {
+            expect(round.toPercent(testCases[0])).to.be.an('array')
+        })
 
-    it('toPercent should sum to 100', function() {
-        for (let i = 0; i < testCases.length; i++) {
-            expect(round.toPercent(testCases[i]).reduce((acc, elem) => acc + elem, 0)).to.equal(100)
-            expect(round.toPercent(testCases[i],0).reduce((acc, elem) => acc + elem, 0)).to.equal(100)
-            expect(round.toPercent(testCases[i],1).reduce((acc, elem) => acc + elem, 0)).to.equal(100)
-            expect(round.toPercent(testCases[i],2).reduce((acc, elem) => acc + elem, 0)).to.equal(100)
-            expect(round.toPercent(testCases[i],3).reduce((acc, elem) => acc + elem, 0)).to.equal(100)
-        }
-    })
+        it('should return an array of equal length as input', function() {
+            expect(round.toPercent(testCases[0])).to.be.length(testCases[0].length)
+        })
 
-    it('toPercent with precision should return array with correct amount of decimal places', function() {
-        for (let i = 0; i < testCases.length; i++) {
-            let test = round.toPercent(testCases[i], 2)
-
-            for (let j = 0; j < test.length; j++) {
-                let split = test[j].toString().split('.')
-                if (split.length == 2) expect(split[1].length).to.be.lte(2)
+        it('should sum to 100', function() {
+            for (let i = 0; i < testCases.length; i++) {
+                expect(round.sumWithPrecision(round.toPercent(testCases[i]))).to.equal(100)
+                expect(round.sumWithPrecision(round.toPercent(testCases[i],0))).to.equal(100)
+                expect(round.sumWithPrecision(round.toPercent(testCases[i],1))).to.equal(100)
+                expect(round.sumWithPrecision(round.toPercent(testCases[i],2))).to.equal(100)
+                expect(round.sumWithPrecision(round.toPercent(testCases[i],3))).to.equal(100)
             }
-        }
+        })
+
+        it('should return array with correct amount of decimal places when precision argument passed', function() {
+            for (let i = 0; i < testCases.length; i++) {
+                let test = round.toPercent(testCases[i], 2)
+
+                for (let j = 0; j < test.length; j++) {
+                    let split = test[j].toString().split('.')
+                    if (split.length == 2) expect(split[1].length).to.be.lte(2)
+                }
+            }
+        })
+    })
+    
+    describe('toAbsolute', function() {
+        const testCases = [
+            { total: 500, spread: [50,50] },
+            { total: 250, spread: [10, 12, 14, 16, 18, 20, 10] },
+            { total: 210, spread: [0.002, 99.998] },
+            { total: 1230, spread: [21.2, 12.4, 8.7, 9.326, 8.1, 12.7863144, 8, 10, 9.4876856] },
+            { total: 150, spread: [33.33333333, 33.33333333, 33.33333334] }
+        ]
+
+        it('should be a function', function() {
+            expect(round.toAbsolute).to.be.a('function')
+        })
+
+        it('should return an array', function() {
+            expect(round.toAbsolute(testCases[0].total, testCases[0].spread)).to.be.an('array')
+        })
+
+        it('should return an array of equal length as input', function() {
+            expect
+        })
     })
 })
