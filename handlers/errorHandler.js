fe89ed481aab776ad9020b227f5f52e520b78801
Håkon Harnes <account@harnes.me>
2017-08-14 14:13:40 +0200

    Compact error handler function

--- handlers/errorHandler.js
+++ handlers/errorHandler.js
@@ -8,8 +8,7 @@ module.exports = (err, req, res, next) => {
     if (!err.status) err.status = 500
     if (!err.msg) err.msg = "Internal server error"
 
-    res.status(err.status)
-    res.json({
+    res.status(err.status).json({
         status: err.status,
         content: err.msg
     })
