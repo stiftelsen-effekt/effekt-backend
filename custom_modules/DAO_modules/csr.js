7cf7694faba81d2b1be02aa79ced60e7b3e6d8ea
Vegard <v.v.blindheim@gmail.com>
2017-11-24 11:45:02 +0100

    Morebones CSR-backend, upd. node version in package.json

--- custom_modules/DAO_modules/csr.js
+++ custom_modules/DAO_modules/csr.js
@@ -1,21 +1,31 @@
-const sqlString  = require('sqlString')
-const DAO = require('../DAO')
-
 var con
 
 // Get all donations by id
 function getAllDonationsByDonorId(donorId) {
     return new Promise(async (resolve, reject) => {
         try {
-            var [res] = await con.query('SELECT * FROM EffektDonasjonDB.Donations WHERE DonorID = ?', [donorId])
-            resolve(res)
+            var [res] = await con.query('SELECT * FROM EffektDonasjonDB.Donations WHERE Donor_ID = ?', [donorId])
+            // Kan hente bare sum_confirmed?
+            var total = 0
+            if (res.length > 0) {
+                total = res.reduce((accumulator, current) => {
+                    return accumulator + Number.parseInt(current.sum_confirmed)
+                }, 0)
+            }
+
+            resolve(total)
         }
         catch (exception) {
+            console.log(exception)
             reject(exception)
         }
     })
 }
 
-module.exports = {
-    getAllDonationsByDonorId,
+module.exports = function(dbPool) {
+    con = dbPool
+
+    return {
+        getAllDonationsByDonorId,
+    }
 }
--- routes/csr.js
+++ routes/csr.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-const csr = require('../custom_modules/DAO_modules/csr')
+const DAO = require('../custom_modules/DAO.js')
 const GDLiveParser = require('../custom_modules/GDLive_parser')
 
 const bodyParser = require('body-parser')
@@ -9,8 +9,7 @@ const urlEncodeParser = bodyParser.urlencoded({ extended: false })
 
 router.get("/donations/:id", urlEncodeParser, async (req, res, next) => {
     try {
-        //var total = 20045 // kan bruke DAO.donations.getFullDonationByDonor() ??
-        var total = csr.getAllDonationsByDonorID(req.params.id)
+        var total = await DAO.csr.getAllDonationsByDonorId(req.params.id)
 
         res.json({
             status: 200,
@@ -22,15 +21,14 @@ router.get("/donations/:id", urlEncodeParser, async (req, res, next) => {
     }
 })
 
-router.get("/donations/gdlive", urlEncodeParser, async (req, res, next) => {
+router.get("/gdlive/:number", urlEncodeParser, async (req, res, next) => {
     try {
-        var json = GDLiveParser.getJSON()
+        var json = await GDLiveParser.getJSON(req.params.number)
 
         res.json({
             status: 200,
             content: json
         })
-
     }
     catch (exception) {
         next({exception: exception})
