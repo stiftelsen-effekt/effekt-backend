081195d207e28e1a7b5adf092e50cb6e904d88fd
Vegard Blindheim <vegardvb@stud.ntnu.no>
2018-03-27 19:36:00 +0200

    Fiks?

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,4 +3,7 @@
 
 # find . -not -name "node_modules/" -exec curl -k --ftp-create-dirs -T {} -u $FTP_USER:$FTP_PASSWORD sftp://api.gieffektivt.no/var/www/api \;
 
+# find . -path ./node_modules -prune -o -type d
+
 sftp -o StrictHostKeyChecking=no -b ./deploy/sftp_batch.txt gieffektivt@api.gieffektivt.no
+
