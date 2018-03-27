80062b6c43f6d4af2f0ab3ad1196e97f36bcb54d
Vegard Blindheim <vegardvb@stud.ntnu.no>
2018-03-27 16:43:15 +0200

    Retting av feil i deploy-skript.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Deployment-skript som kjøres av Travis etter suksessfull build av production-branchen
 
-find . -exec curl --ftp-create-dirs -T {} -u $FTP_USER:$FTP_PASSWORD sftp://api.gieffektivt.no/var/www/api -k
+find . -exec curl -k --ftp-create-dirs -T {} -u $FTP_USER:$FTP_PASSWORD sftp://api.gieffektivt.no/var/www/api \;
