1d074a872d964fb6bd0388dd7026616a7f6712c2
Håkon Harnes <sosser@skymiastudios.com>
2018-03-28 13:14:56 +0200

    Another update to deploy script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,4 +2,4 @@
 # Deployment-skript som kjøres av Travis etter suksessfull build av production-branchen
 
 rsync -r -e "ssh -o StrictHostKeyChecking=no" $TRAVIS_BUILD_DIR/ $FTP_USER@api.gieffektivt.no:/var/www/api/
-ssh $FTP_USER@api.gieffektivt.no "pm2 restart all;"
+ssh $FTP_USER@api.gieffektivt.no "pm2 restart all; exit;"
