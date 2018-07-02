9d15b07e5528102fbc12526496249cecb456113f
Håkon Harnes <sosser@skymiastudios.com>
2018-07-02 16:30:01 +0200

    Fix major security voulnerabilities

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,22 @@ const gulp = require('gulp')
 const sass = require('gulp-sass')
 const watch = require('gulp-watch')
 
-gulp.task('style', function() {
+gulp.task('style', function(done) {
     gulp.src('views/style/**/*.scss')
         .pipe(sass({
             errLogToConsole: true
         }))
         .pipe(gulp.dest('views/style/'))
+
+    done()
 })
 
-gulp.task('default', ['style'])
+gulp.task('watch', function(done) {
+    gulp.watch('views/style/**/*.scss').on("change", (path, stats) => {
+        gulp.series('style');
+    })
 
-gulp.task('watch', function() {
-    gulp.watch('views/style/**/*.scss', ['style'])
+    done()
 })
+
+gulp.task('default', gulp.series('style'))
