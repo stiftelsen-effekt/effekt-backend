d1afa55fd7e51e66353045b1bfeb7ca77d703198
Håkon Harnes <hh@eiendomsverdi.no>
2018-12-11 18:38:34 +0100

    Updates the reporting excel file with summation for vipps and paypal, as well as a checksum

--- custom_modules/reporting.js
+++ custom_modules/reporting.js
@@ -9,22 +9,43 @@ module.exports = {
      */
     createExcelFromIndividualDonations: function(donations, organizations) {
         let organizationMapping = new Map();
+        const dataStartRow = 6;
 
         //A 2-dimensional array representing rows and columns
         let data = []
 
         //Generate headers for data
-        let dataTopRow =    ['ID',  'Donasjon registrert',  'Navn',     'Metode',   'Sum']
-        let dataSumation =  ['',    '',                     '',         '',         'SUM:']
+        let sumifnameColumn = COLUMN_MAPPING[3] //Metode header
+
+        let simifcomparisonrange = `${sumifnameColumn + dataStartRow}:${sumifnameColumn + (donations.length+dataStartRow)}`
+        let sumationRange = `${COLUMN_MAPPING[4] + dataStartRow}:${COLUMN_MAPPING[4] + (donations.length+dataStartRow)}`
+        let checkSumRange = `${COLUMN_MAPPING[6]}1:${COLUMN_MAPPING[6 + (organizations.length * 3)]}1`;
+
+        let dataTopRow =            ['ID',  'Donasjon registrert',  'Navn',     'Metode',   'Sum']
+        let dataSumation =          ['Checksum', formula(`${COLUMN_MAPPING[4]}1 - SUM(${checkSumRange})`), '','Sum',formula(`SUM(${sumationRange})`)]
+
+        //Sumation for specific payment methods
+        
+        let dataSumationPayPal =    ['',    '',                     '',         'Sum PayPal',formula(`SUMIF(${simifcomparisonrange}, "PayPal", ${sumationRange})`)]
+        let dataSumationVipps =     ['',    '',                     '',         'Sum Vipps', formula(`SUMIF(${simifcomparisonrange}, "Vipps", ${sumationRange})`)]
+
         let currentColumn = dataTopRow.length
 
+        
+
         organizations.forEach((org) => {
-            let organizationHeaders = [org.name, 'Prosent fordeling', 'Sum fordeling']
+            let organizationHeaders = [org.name, '%', 'Kr']
             dataTopRow.push(...organizationHeaders)
 
             let sumationColumn = COLUMN_MAPPING[dataSumation.length+2]
-            let organizationSumColumns = ['', org.name, {v: '', f: `=SUM(${sumationColumn}4:${sumationColumn + (donations.length+4)})`}]
+            let sumationRange = `${sumationColumn + dataStartRow}:${sumationColumn + (donations.length+dataStartRow)}`;
+
+            let organizationSumColumns = [org.abbriv, '', formula(`SUM(${sumationRange})`)]
             dataSumation.push(...organizationSumColumns)
+            let organizationSumPayPalColumns = [org.abbriv, '', formula(`SUMIF(${simifcomparisonrange}, "PayPal", ${sumationRange})`)]
+            dataSumationPayPal.push(...organizationSumPayPalColumns)
+            let organizationSumVippsColumns = [org.abbriv, '', formula(`SUMIF(${simifcomparisonrange}, "Vipps", ${sumationRange})`)]
+            dataSumationVipps.push(...organizationSumVippsColumns)
 
             organizationMapping.set(org.id, currentColumn)
             currentColumn += organizationHeaders.length
@@ -48,6 +69,8 @@ module.exports = {
 
         //Add all the generated data
         data.push(dataSumation)
+        data.push(dataSumationPayPal)
+        data.push(dataSumationVipps)
         data.push([]) //Spacing row
         data.push(dataTopRow)
         data.push(...dataRows)
@@ -55,7 +78,14 @@ module.exports = {
         let buffer = xlsx.build([{name: "mySheetName", data: data}]);
 
         return buffer;
+
+        //Helper functions
+        function formula(formula) {
+            return {v: '', f: `=${formula}`}
+        }
     }
 }
 
-const COLUMN_MAPPING = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'AA', 'AB', 'AC', 'AD', 'AE', 'AF', 'AG', 'AH', 'AI', 'AJ', 'AK', 'AL', 'AM', 'AN', 'AO', 'AP', 'AQ', 'AR', 'AS']
+const COLUMN_MAPPING = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 
+                        'AA', 'AB', 'AC', 'AD', 'AE', 'AF', 'AG', 'AH', 'AI', 'AJ', 'AK', 'AL', 'AM', 'AN', 'AO', 'AP', 'AQ', 'AR', 'AS', 'AT', 'AU', 'AV', 'AW', 'AX', 'AY', 'AZ',
+                        'BA', 'BB', 'BC', 'BD', 'BE', 'BF', 'BG', 'BH', 'BI', 'BJ', 'BK', 'BL', 'BM', 'BN', 'BO', 'BP', 'BQ', 'BR', 'BS', 'BT', 'BU', 'BV', 'BW', 'BX', 'BY', 'BZ' ]
