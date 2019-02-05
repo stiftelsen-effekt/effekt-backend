0e883683448d4be0d83d4bc04e93a5fb4a9ae670
Håkon Egset Harnes <account@harnes.me>
2019-02-05 16:57:06 +0100

    Reporting: Removes percentage and organization-name from individual donation rows, adds a gross and net column

--- custom_modules/reporting.js
+++ custom_modules/reporting.js
@@ -3,10 +3,10 @@ const xlsx = require('node-xlsx').default
 module.exports = {
     /**
      * Creates an excel file from individual donations
-     * @param {Array} donations An array containing individual donations
-     * @param {Array} organizations An array of all the organizations in the database
-     * @param {Array} paymentMethods An array of paymentmethods in the donation-list
-     * @returns {Buffer} The output excel file
+     * @param {Array<Donation>} donations An array containing individual donations
+     * @param {Array<Organization>} organizations An array of all the organizations in the database
+     * @param {Array<PaymentMethod>} paymentMethods An array of paymentmethods in the donation-list
+     * @returns {Buffer} The output excel file, as a buffer
      */
     createExcelFromIndividualDonations: function(donations, organizations, paymentMethods) {
         let organizationMapping = new Map();
@@ -15,18 +15,26 @@ module.exports = {
         //A 2-dimensional array representing rows and columns
         let data = []
 
-        //Generate headers for data
+        //Convenience variables defining commonly used excel ranges
         let sumifnameColumn = COLUMN_MAPPING[3] //Metode header
 
         let sumifcomparisonrange = `${sumifnameColumn + dataStartRow}:${sumifnameColumn + (donations.length+dataStartRow)}`
-        let sumationRange =     `${COLUMN_MAPPING[4] + dataStartRow}:${COLUMN_MAPPING[4] + (donations.length+dataStartRow)}`
+        let sumationGrossRange =`${COLUMN_MAPPING[4] + dataStartRow}:${COLUMN_MAPPING[4] + (donations.length+dataStartRow)}`
         let sumationFeesRange = `${COLUMN_MAPPING[5] + dataStartRow}:${COLUMN_MAPPING[5] + (donations.length+dataStartRow)}`
-        let checkSumRange =     `${COLUMN_MAPPING[7]}1:${COLUMN_MAPPING[7 + (organizations.length * 3)]}1`;
+        let sumationNetRange =  `${COLUMN_MAPPING[6] + dataStartRow}:${COLUMN_MAPPING[6] + (donations.length+dataStartRow)}`
+        let checkSumRange =     `${COLUMN_MAPPING[7]}1:${COLUMN_MAPPING[7 + (organizations.length)]}1`;
 
-        let dataTopRow =            ['ID',  'Donasjon registrert',  'Navn',     'Metode',   'Sum', 'Avgifter']
+        //Generate headers for data
+        let dataTopRow =            ['ID',  'Donasjon registrert',  'Navn',     'Metode',   'Brutto', 'Avgifter', 'Netto']
         
         //Sumation-rows
-        let dataSumation =          ['Checksum', formula(`${COLUMN_MAPPING[4]}1 - SUM(${checkSumRange})`), '','Sum',formula(`SUM(${sumationRange})`), formula(`SUM(${sumationFeesRange})`)]
+        let dataSumation =         ['Checksum', 
+                                    formula(`${COLUMN_MAPPING[4]}1 - SUM(${checkSumRange})`), 
+                                    '',
+                                    'Sum',
+                                    formula(`SUM(${sumationGrossRange})`), 
+                                    formula(`SUM(${sumationFeesRange})`),
+                                    formula(`SUM(${sumationNetRange})`)]
             //Sumation for specific payment methods
             let methodSumationRows = []
             paymentMethods.forEach((method) => {
@@ -34,26 +42,29 @@ module.exports = {
                                             formula(`COUNTIF(D${dataStartRow}:D1000,"${method.name}")`),
                                             '',
                                             `Sum ${method.name}`,
-                                            formula(`SUMIF(${sumifcomparisonrange}, "${method.name}", ${sumationRange})`), 
-                                            formula(`SUMIF(${sumifcomparisonrange}, "${method.name}", ${sumationFeesRange})`)])
+                                            formula(`SUMIF(${sumifcomparisonrange}, "${method.name}", ${sumationGrossRange})`), 
+                                            formula(`SUMIF(${sumifcomparisonrange}, "${method.name}", ${sumationFeesRange})`),
+                                            formula(`SUMIF(${sumifcomparisonrange}, "${method.name}", ${sumationNetRange})`)
+                                        ])
+                                             
             })
         
 
         let currentColumn = dataTopRow.length
 
         organizations.forEach((org) => {
-            let organizationHeaders = [org.name, '%', 'Kr']
+            let organizationHeaders = [org.abbriv]
             dataTopRow.push(...organizationHeaders)
 
-            let sumationColumn = COLUMN_MAPPING[dataSumation.length+2]
+            let sumationColumn = COLUMN_MAPPING[dataSumation.length]
             let sumationRange = `${sumationColumn + dataStartRow}:${sumationColumn + (donations.length+dataStartRow)}`;
 
-            let organizationSumColumns = [org.abbriv, '', formula(`SUM(${sumationRange})`)]
+            let organizationSumColumns = [formula(`SUM(${sumationRange})`)]
             dataSumation.push(...organizationSumColumns)
 
             //Add sumation for each organization filtered on each payment method
             paymentMethods.forEach((method, i) => {
-                let organizationSumMethodColumns = [org.abbriv, '', formula(`SUMIF(${sumifcomparisonrange}, "${method.name}", ${sumationRange})`)]
+                let organizationSumMethodColumns = [formula(`SUMIF(${sumifcomparisonrange}, "${method.name}", ${sumationRange})`)]
                 methodSumationRows[i].push(...organizationSumMethodColumns)
             })
 
@@ -64,27 +75,32 @@ module.exports = {
         //Generate the actual donation data
         let dataRows = []
         donations.forEach((donation) => {
-            let donationRow = [donation.ID, donation.time, donation.name, donation.paymentMethod, donation.sum, Number(donation.transactionCost)]
+            let donationRow = [ donation.ID, 
+                                donation.time, 
+                                donation.name, 
+                                donation.paymentMethod, 
+                                donation.sum, 
+                                Number(donation.transactionCost),
+                                roundCurrency(donation.sum-Number(donation.transactionCost))]
 
+            //For each organization in donation
             donation.split.forEach((split) => {
                 let startIndex = organizationMapping.get(split.id)
 
-                donationRow[startIndex] = split.name;
-                donationRow[startIndex+1] = Number(split.percentage);
-                donationRow[startIndex+2] = roundCurrency(Number(split.amount));
+                donationRow[startIndex] = roundCurrency(Number(split.amount));
             })
 
             dataRows.push(donationRow)
         })
 
         //Add all the generated data
-        data.push(dataSumation)
-        data.push(...methodSumationRows)
-        data.push([]) //Spacing row
-        data.push(dataTopRow)
-        data.push(...dataRows)
+        data.push(dataSumation)             //Overall sumation
+        data.push(...methodSumationRows)    //Sumation for all the payment methods
+        data.push([])                       //Spacing row
+        data.push(dataTopRow)               //Headers for individual donations
+        data.push(...dataRows)              //All the individual donations
 
-        let buffer = xlsx.build([{name: "mySheetName", data: data}]);
+        let buffer = xlsx.build([{name: "Donations", data: data}]);
 
         return buffer;
 
