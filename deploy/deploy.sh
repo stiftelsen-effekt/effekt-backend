b4fd38ab60e6105395ca831de562694265e41f1b
Håkon Harnes <hh@eiendomsverdi.no>
2020-04-17 14:29:40 +0200

    Removes legacy files

--- deploy/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Deployment-skript som kjøres av Travis etter suksessfull build av production-branchen
-
-rsync -r --update -e "ssh -o StrictHostKeyChecking=no" $TRAVIS_BUILD_DIR/ $FTP_USER@api.gieffektivt.no:/var/www/api/
-ssh $FTP_USER@api.gieffektivt.no "npm install; pm2 reload all; exit;"
