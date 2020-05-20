4b0fd7b3ffe539aa5bee6685327df78315770637
Håkon Harnes <hh@eiendomsverdi.no>
2020-05-20 22:33:51 +0200

    Adds config variables

--- config.js
+++ config.js
@@ -13,6 +13,11 @@ module.exports = {
     mailchimp_api_key: process.env.MAILCHIMP_API_KEY,
     mailchimp_audience_id: process.env.MAILCHIMP_AUDIENCE_ID,
 
+    vipps_client_id: process.env.VIPPS_CLIENT_ID,
+    vipps_client_secret: process.env.VIPPS_CLIENT_SECRET,
+    vipps_ocp_apim_subscription_key: process.env.VIPPS_OCP_APIM_SUBSCRIPTION_KEY,
+    vipps_merchant_serial_number: process.env.VIPPS_MERCHANT_SERIAL_NUMBER,
+
     //Set port for API listening, default to 3000
     port: process.env.PORT || 3000,
 
