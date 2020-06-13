2f9e113fef09d7b64ea4854d5db80d3cc1c505e1
philiphand <philip.h.andersen@gmail.com>
2020-06-13 13:11:13 +0200

    add philip route

--- server.js
+++ server.js
@@ -110,6 +110,7 @@ DAO.connect(() => {
   const authRoute =           require('./routes/auth')
   const metaRoute =           require('./routes/meta')
   const debugRoute =          require('./routes/debug')
+  const philipRoute =         require('./routes/philip')
 
   app.use('/donors',        donorsRoute)
   app.use('/donations',     donationsRoute)
@@ -123,6 +124,7 @@ DAO.connect(() => {
   app.use('/auth',          authRoute)
   app.use('/meta',          metaRoute)
   app.use('/debug',         debugRoute)
+  app.use('/philip',         philipRoute)
 
   app.use('/static',  express.static(__dirname + '/static'))
   app.use('/style',   express.static(__dirname + '/views/style'))
