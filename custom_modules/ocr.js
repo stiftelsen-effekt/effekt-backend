0499b45db8478ad5bf2e9112c8bc74dc9b11efc0
Håkon Harnes <account@harnes.me>
2021-05-26 19:05:06 +0200

    Fixes OCR donation insertions

--- custom_modules/ocr.js
+++ custom_modules/ocr.js
@@ -24,20 +24,24 @@ module.exports = {
    * @returns {AddDonationsResult}
    */
   async addDonations(transactions, metaOwnerID) {
-    const tasks = transactions.map(transaction => this.addDonation(transaction, metaOwnerID))
+    /**
+     * @type {Array<AddDonationResult>}
+     */
+    const results = []
 
-    //Add in paralell
-    const results = await Promise.allSettled(tasks)
-
-    const valid = results.filter(result => result.status === 'fulfilled' && result.value === true).length
-    const ignored = results.filter(result => result.status === 'fulfilled' && result.value === -1).length
-    const invalidTransactions = results.filter(result => result.status === 'fulfilled' && typeof result.value === 'object').map(result => result.value)
-    const failed = results.filter(result => result.status === 'rejected')
-
-    for (let i = 0; i < failed.length; i++) {
-      console.error(`Failed to add OCR donation`)
-      console.errror(failed[i].reason)
+    for (let i = 0; i < transactions.length; i++) {
+      const transaction = transactions[i]
+      const result = await this.addDonation(transaction, metaOwnerID)
+      results.push(result)
     }
+
+    const addedIDs = results.filter(result => (typeof result !== 'object' && result > 0))
+ 
+    await this.sendReciepts(addedIDs)
+
+    const valid = addedIDs.length
+    const ignored = results.filter(result => result === -1).length
+    const invalidTransactions = results.filter(result => typeof result === 'object')
 
     return {
       valid,
@@ -48,26 +52,47 @@ module.exports = {
   },
   
   /**
+   * @typedef {number | -1 | InvalidTransaction} AddDonationResult
+   */
+
+  /**
    * Adds transactions parced from OCR to the database
    * @param {Array<import('./parsers/OCR.js').OCRTransaction>} transactions 
    * @param {number} metaOwnerID 
-   * @returns {true | -1 | InvalidTransaction} True if added, -1 if ignored, object if failed
+   * @returns {AddDonationResult} DonationID if added, -1 if ignored, object if failed
    */
   async addDonation(transaction, metaOwnerID) {
     try {
       let donationID = await DAO.donations.add(transaction.KID, transaction.paymentMethod, transaction.amount, transaction.date, transaction.transactionID, metaOwnerID)
-      if (config.env === 'production') await mail.sendDonationReciept(donationID)
-      return true
+      return donationID
     } catch (ex) {
       //Only return a failed if it failed because the donation exists
       if (ex.message.indexOf("EXISTING_DONATION") == -1) {
-        return {
-            reason: ex.message,
-            transaction
+      return {
+          reason: ex.message,
+          transaction
         }
       } else {
         console.log(`Attempted to add existing donation with KID ${transaction.KID}`)
         return -1
+      }
+    }
+  },
+
+  /**
+   * Sends donation reciepts in paralell
+   * @param {Array<number>} donationIDs 
+   */
+  async sendReciepts(donationIDs) {
+    //Send mail in paralell
+    if (config.env === 'production') {
+      const tasks = donationIDs.map(id => mail.sendDonationReciept(id))
+      const results = await Promise.allSettled(tasks)
+      const failed = results.filter(result => result.status === 'rejected')
+
+      for (let i = 0; i < failed.length; i++) {
+        console.error(`Failed to send donation reciept for donation`)
+        console.errror(failed[i].reason)
       }
     }
   }
