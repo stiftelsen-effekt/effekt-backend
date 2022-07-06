223ad904800f5e81e46183dcb4ee750f036d7df5
Håkon Harnes <account@harnes.me>
2022-07-06 12:45:29 +0200

    Hotfix for AvtaleGiro generation when donor has no name

--- src/custom_modules/avtalegiro/filewriterutil.js
+++ src/custom_modules/avtalegiro/filewriterutil.js
@@ -1,167 +1,191 @@
-const config = require('../../config')
-const luxon = require('luxon');
-const { DateTime } = require('luxon');
+const config = require("../../config");
+const luxon = require("luxon");
+const { DateTime } = require("luxon");
 
 module.exports = {
-  startRecordTransmission: function(shipmentID) { 
+  startRecordTransmission: function (shipmentID) {
     let customerId = config.nets_customer_id;
 
-    var line = `NY000010${customerId.padStart(8,'0')}${shipmentID.toString().padStart(7,'0')}00008080`
-    line = line.padEnd(80, '0')
-    line += '\n'
+    var line = `NY000010${customerId.padStart(8, "0")}${shipmentID
+      .toString()
+      .padStart(7, "0")}00008080`;
+    line = line.padEnd(80, "0");
+    line += "\n";
     return line;
   },
 
-  startRecordPaymentAssignment: function(currentDate, number) {
-    var line =`NY210020`
-    line = line.padEnd(17, '0')
+  startRecordPaymentAssignment: function (currentDate, number) {
+    var line = `NY210020`;
+    line = line.padEnd(17, "0");
 
     if (number > 999)
-      throw new Error("Cannot handle more than 999 assignments per file")
+      throw new Error("Cannot handle more than 999 assignments per file");
 
     // Oppdragsnr.
-    line += currentDate.toFormat("ddLL") + `${number}`.padStart(3,'0')
+    line += currentDate.toFormat("ddLL") + `${number}`.padStart(3, "0");
     // Accountnr.
-    line += '15062995960'
-    line = line.padEnd(80, '0')
+    line += "15062995960";
+    line = line.padEnd(80, "0");
 
-    line += '\n'
-    return line
+    line += "\n";
+    return line;
   },
 
   /**
-   * 
-   * @param {import('../parsers/avtalegiro').AvtalegiroAgreement} agreement 
-   * @param {import('../DAO_modules/donors').Donor} donor 
-   * @param {string} type 
-   * @param {number} transactionNumber 
-   * @param {DateTime} claimDate 
+   *
+   * @param {import('../parsers/avtalegiro').AvtalegiroAgreement} agreement
+   * @param {import('../DAO_modules/donors').Donor} donor
+   * @param {string} type
+   * @param {number} transactionNumber
+   * @param {DateTime} claimDate
    * @returns {string}
    */
-  firstAndSecondLine: function(agreement, donor, type, transactionNumber, claimDate) {
+  firstAndSecondLine: function (
+    agreement,
+    donor,
+    type,
+    transactionNumber,
+    claimDate
+  ) {
     /**
      * First line
      */
-    var firstLine =`NY21${type}30${transactionNumber.toString().padStart(7,'0')}`
-    firstLine += claimDate.toFormat("ddLLyy")
-    firstLine = firstLine.padEnd(32, '0')
+    var firstLine = `NY21${type}30${transactionNumber
+      .toString()
+      .padStart(7, "0")}`;
+    firstLine += claimDate.toFormat("ddLLyy");
+    firstLine = firstLine.padEnd(32, "0");
 
-    var amount = agreement.amount
-    amount = amount.toString().padStart(17, '0')
-    firstLine += amount
+    var amount = agreement.amount;
+    amount = amount.toString().padStart(17, "0");
+    firstLine += amount;
 
-    var KID = agreement.KID
-    KID = KID.toString().padStart(25, ' ')
-    firstLine += KID
+    var KID = agreement.KID;
+    KID = KID.toString().padStart(25, " ");
+    firstLine += KID;
 
-    firstLine = firstLine.padEnd(80, '0')
-    firstLine += '\n'
+    firstLine = firstLine.padEnd(80, "0");
+    firstLine += "\n";
 
     /**
      * Second line
      */
-    const shortname = donor.name.toUpperCase().substr(0,10).replace(/\s+/g, '').padStart(10, ' ')
+    const shortname = (
+      donor.name
+        ? donor.name.toUpperCase().substr(0, 10).replace(/\s+/g, "")
+        : ""
+    ).padStart(10, " ");
 
-    var secondLine =`NY210231${transactionNumber.toString().padStart(7,'0')}${shortname}`
+    var secondLine = `NY210231${transactionNumber
+      .toString()
+      .padStart(7, "0")}${shortname}`;
 
-    secondLine = secondLine.padEnd(75, ' ')
-    secondLine = secondLine.padEnd(80, '0')
+    secondLine = secondLine.padEnd(75, " ");
+    secondLine = secondLine.padEnd(80, "0");
 
     /**
      * Combine lines
      */
-    lines = `${firstLine}${secondLine}`
-    lines += '\n'
-    return lines
+    lines = `${firstLine}${secondLine}`;
+    lines += "\n";
+    return lines;
   },
 
   /**
-   * 
-   * @param {import('../parsers/avtalegiro').AvtalegiroAgreement} claims 
+   *
+   * @param {import('../parsers/avtalegiro').AvtalegiroAgreement} claims
    * @param {DateTime} minDate Minimum claim date
    * @param {DateTime} maxDate Maximum claim date
-   * @returns 
+   * @returns
    */
-  endRecordPaymentAssignment: function(claims, minDate, maxDate) {
-    var line =`NY210088`
+  endRecordPaymentAssignment: function (claims, minDate, maxDate) {
+    var line = `NY210088`;
 
     //Number of transactions
-    line += claims.length.toString().padStart(8,'0')
+    line += claims.length.toString().padStart(8, "0");
 
     //Number of records, including start and end record
-    line += (claims.length*2+2).toString().padStart(8,'0')
+    line += (claims.length * 2 + 2).toString().padStart(8, "0");
 
     //Sum of payment claims
-    line += claims.reduce((acc, claim) => acc += claim.amount, 0).toString().padStart(17, '0')
+    line += claims
+      .reduce((acc, claim) => (acc += claim.amount), 0)
+      .toString()
+      .padStart(17, "0");
 
     //Min day
-    line += minDate.toFormat("ddLLyy")
+    line += minDate.toFormat("ddLLyy");
 
     //Max day
-    line += maxDate.toFormat("ddLLyy")
+    line += maxDate.toFormat("ddLLyy");
 
-    line = line.padEnd(80, '0')
-    line += '\n'
-    return line
+    line = line.padEnd(80, "0");
+    line += "\n";
+    return line;
   },
 
-  startRecordDeletionRequest: function() {
-    var line =`NY213620`
-    line = line.padEnd(17, '0')
+  startRecordDeletionRequest: function () {
+    var line = `NY213620`;
+    line = line.padEnd(17, "0");
     // Oppdragsnr.
-    line += '2'.padStart(7, '0')
+    line += "2".padStart(7, "0");
     // Accountnr.
-    line += '15062995960'
-    line = line.padEnd(80, '0')
+    line += "15062995960";
+    line = line.padEnd(80, "0");
 
-    line += '\n'
-    return line
+    line += "\n";
+    return line;
   },
 
-  endRecordDeletionRequest: function(dueDate) {
-    var line =`NY213688`
+  endRecordDeletionRequest: function (dueDate) {
+    var line = `NY213688`;
 
     //Number of transactions
-    line += '0'.padStart(8,'0')
+    line += "0".padStart(8, "0");
 
     //Number of records, including start and end record
-    line += '2'.padStart(8,'0')
+    line += "2".padStart(8, "0");
 
     //Sum of deletion requests amount
-    line += '0'.padStart(17, '0')
+    line += "0".padStart(17, "0");
 
-    const minMaxDate = dueDate.toFormat("ddLLyy")
+    const minMaxDate = dueDate.toFormat("ddLLyy");
 
     //Min day
-    line += minMaxDate
+    line += minMaxDate;
 
     //Max day
-    line += minMaxDate
+    line += minMaxDate;
 
-    line = line.padEnd(80, '0')
-    line += '\n'
-    return line
+    line = line.padEnd(80, "0");
+    line += "\n";
+    return line;
   },
 
-  endRecordTransmission: function(claims, deletions, minDate) {
-    var line =`NY000089`
+  endRecordTransmission: function (claims, deletions, minDate) {
+    var line = `NY000089`;
 
     //Number of transactions
-    line += (claims.length + deletions.length).toString().padStart(8,'0')
+    line += (claims.length + deletions.length).toString().padStart(8, "0");
 
     //Number of records, including start and end record
-    line += (claims.length*4 + deletions.length * 4 + 2).toString().padStart(8,'0')
+    line += (claims.length * 4 + deletions.length * 4 + 2)
+      .toString()
+      .padStart(8, "0");
 
     //Sum of payment claims
-    let claimSum = claims.reduce((acc, claim) => acc += claim.amount, 0)
-    let deletionsSum = deletions.reduce((acc, deletion) => acc += deletion.amount, 0)
-    line += (claimSum + deletionsSum).toString().padStart(17, '0')
+    let claimSum = claims.reduce((acc, claim) => (acc += claim.amount), 0);
+    let deletionsSum = deletions.reduce(
+      (acc, deletion) => (acc += deletion.amount),
+      0
+    );
+    line += (claimSum + deletionsSum).toString().padStart(17, "0");
 
     //Min day
-    line += minDate.toFormat("ddLLyy")
+    line += minDate.toFormat("ddLLyy");
 
-    line = line.padEnd(80, '0')
-    line += '\n'
-    return line
-  }
-}
+    line = line.padEnd(80, "0");
+    line += "\n";
+    return line;
+  },
+};
