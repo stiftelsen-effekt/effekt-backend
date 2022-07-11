f064a2c22dc703fe5a44fec94db295883a993ca7
Philip <44257880+philiphand@users.noreply.github.com>
2022-07-11 23:36:34 +0200

    Adds mockAgreements and mockCharges

--- src/__test__/vipps.js
+++ src/__test__/vipps.js
@@ -12,25 +12,83 @@ const config = require('../config');
 const authMiddleware = require('../custom_modules/authorization/authMiddleware');
 
 describe('POST /scheduled/vipps', function() {
-  const mockAgreements = [{
-    id: 1,
+  const mockAgreementsVipps = [{
+    ID: "agr_2FDUs6w",
     KID: '002556289731589',
     chargeDay: 10,
     amount: 50000,
     status: "ACTIVE"
   }, {
-    id: 2,
+    ID: "agr_2fh9Huw",
     KID: '000638723319577',
     chargeDay: 0,
     amount: 340000,
     status: "ACTIVE"
   }, {
-    id: 3,
-    KID: '000675978627833',
+    ID: "agr_2NfNnTf",
     chargeDay: 1,
     amount: 5000000,
-    status: "ACTIVE"
+    status: "STOPPED"
   }]
+
+  const mockAgreementsDB = [{
+    ID: "agr_2fh9Huw",
+    donorID: 973,
+    KID: '000638723319577',
+    chargeDay: 0,
+    amount: 3400,
+    status: "ACTIVE"
+  }, {
+    ID: "agr_2NfNnTf",
+    donorID: 973,
+    KID: '000675978627833',
+    chargeDay: 1,
+    amount: 50000,
+    status: "STOPPED"
+  }]
+
+  const mockChargesVipps = [
+    {
+      id: "chr-2cXEgFt",
+      amount: "5000",
+      due: "2022-07-13",
+      status: "CHARGED",
+      type: "INITIAL"
+    },
+    {
+      id: "chr-",
+      amount: "5000",
+      due: "2022-07-13",
+      status: "PENDING",
+      type: "RECURRING"
+    },
+    {
+      id: "chr-",
+      amount: "5000",
+      due: "2022-07-13",
+      status: "CHARGED",
+      type: "RECURRING"
+    },
+    {
+      id: "chr-2cXEgFt",
+      amount: "5000",
+      due: "2022-07-13",
+      status: "RESERVED",
+      type: "RECURRING"
+    }
+  ]
+
+  const mockChargesDB = [
+    {
+      chargeID: "chr-2cXEgFt",
+      agreementID: "agr_G2NBhQh",
+      KID: "46677414",
+      amountNOK: "50",
+      due: "2022-07-13",
+      status: "CHARGED",
+      type: "INITIAL"
+    }
+  ]
   
   let server
 
@@ -46,6 +104,7 @@ describe('POST /scheduled/vipps', function() {
   let externalPaymentIDExistsStub
   let hasChargedThisMonthStub
   let createChargeStub
+  let loggingStub
 
   before(function () {
     authStub = sinon.replace(authMiddleware, "isAdmin", [])
@@ -58,6 +117,9 @@ describe('POST /scheduled/vipps', function() {
 
     getAgreementStub = sinon
       .stub(DAO.vipps, 'getAgreement')
+
+    getActiveAgreementStub = sinon
+      .stub(DAO.vipps, 'getActiveAgreements')
 
     addAgreementStub = sinon
       .stub(DAO.vipps, 'addAgreement')
@@ -82,11 +144,9 @@ describe('POST /scheduled/vipps', function() {
 
     createChargeStub = sinon
       .stub(vipps, 'createCharge')
-    
-    getAgreementsStub.withArgs("ACTIVE").resolves(mockAgreements)
-    getAgreementsStub.withArgs("PENDING").resolves(mockAgreements)
-    getAgreementsStub.withArgs("STOPPED").resolves(mockAgreements)
-    getAgreementsStub.withArgs("EXPIRED").resolves([])
+
+    loggingStub = sinon
+      .stub(DAO.logging, 'add')
 
     const scheduledRoute = require('../routes/scheduled')
     server = express()
@@ -97,10 +157,27 @@ describe('POST /scheduled/vipps', function() {
     sinon.resetHistory()
   })
 
-  it('Basic test', async function() {
+  it('No agreements test', async function() {
+    getAgreementsStub.withArgs("ACTIVE").resolves([])
+    getAgreementsStub.withArgs("PENDING").resolves([])
+    getAgreementsStub.withArgs("STOPPED").resolves([])
+    getAgreementsStub.withArgs("EXPIRED").resolves([])
 
     const response = await request(server)
-      .post('/scheduled/vipps?date=2021-10-03')
+      .post('/scheduled/vipps')
+      .expect(200)
+
+    expect(true).to.be.true
+  })
+
+  it('Synchronize agreements and charges', async function() {
+      getAgreementsStub.withArgs("ACTIVE").resolves(mockAgreements)
+      getAgreementsStub.withArgs("PENDING").resolves([])
+      getAgreementsStub.withArgs("STOPPED").resolves([])
+      getAgreementsStub.withArgs("EXPIRED").resolves([])
+
+      const response = await request(server)
+      .post('/scheduled/vipps')
       .expect(200)
 
     expect(true).to.be.true
