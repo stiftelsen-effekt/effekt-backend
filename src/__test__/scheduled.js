93357a17042395c37bfa6d4091fccc353255603f
Håkon Harnes <account@harnes.me>
2022-08-23 01:31:43 +0200

    Fix tests

--- src/__test__/scheduled.js
+++ src/__test__/scheduled.js
@@ -1,158 +1,157 @@
-import { DAO } from '../custom_modules/DAO'
-import * as authMiddleware from '../custom_modules/authMiddleware'
+import { DAO } from "../custom_modules/DAO";
+import * as authMiddleware from "../custom_modules/authorization/authMiddleware";
 
-const sinon = require('sinon');
-const chai = require('chai');
+const sinon = require("sinon");
+const chai = require("chai");
 
-const avtalegiro = require('../custom_modules/avtalegiro')
-const mail = require('../custom_modules/mail');
-const nets = require('../custom_modules/nets')
-const express = require('express')
-const request = require('supertest');
-const { expect } = require('chai');
-const config = require('../config');
+const avtalegiro = require("../custom_modules/avtalegiro");
+const mail = require("../custom_modules/mail");
+const nets = require("../custom_modules/nets");
+const express = require("express");
+const request = require("supertest");
+const { expect } = require("chai");
+const config = require("../config");
 
-describe('POST /scheduled/avtalegiro', function() {
-  const mockAgreements = [{
-    id: 1,
-    KID: '002556289731589',
-    claimDate: 10,
-    amount: 50000,
-    notice: true,
-    active: true
-  }, {
-    id: 2,
-    KID: '000638723319577',
-    claimDate: 10,
-    amount: 340000,
-    notice: false,
-    active: true
-  }, {
-    id: 3,
-    KID: '000675978627833',
-    claimDate: 10,
-    amount: 5000000,
-    notice: true,
-    active: true
-  }]
-  
-  let server
+describe("POST /scheduled/avtalegiro", function () {
+  const mockAgreements = [
+    {
+      id: 1,
+      KID: "002556289731589",
+      claimDate: 10,
+      amount: 50000,
+      notice: true,
+      active: true,
+    },
+    {
+      id: 2,
+      KID: "000638723319577",
+      claimDate: 10,
+      amount: 340000,
+      notice: false,
+      active: true,
+    },
+    {
+      id: 3,
+      KID: "000675978627833",
+      claimDate: 10,
+      amount: 5000000,
+      notice: true,
+      active: true,
+    },
+  ];
 
-  let avtalegiroFileStub
-  let sendNotificationStub
-  let shipmentStub
-  let agreementsStub
-  let loggingStub 
-  let sendFileStub
-  let sendMailBackupStub
-  let authStub
+  let server;
+
+  let avtalegiroFileStub;
+  let sendNotificationStub;
+  let shipmentStub;
+  let agreementsStub;
+  let loggingStub;
+  let sendFileStub;
+  let sendMailBackupStub;
+  let authStub;
 
   before(function () {
-    authStub = sinon.replace(authMiddleware, "isAdmin", [])
+    authStub = sinon.replace(authMiddleware, "isAdmin", []);
 
     avtalegiroFileStub = sinon
-      .stub(avtalegiro, 'generateAvtaleGiroFile')
-      .resolves(Buffer.from('', 'utf-8'))
+      .stub(avtalegiro, "generateAvtaleGiroFile")
+      .resolves(Buffer.from("", "utf-8"));
 
     sendNotificationStub = sinon
-      .stub(mail, 'sendAvtalegiroNotification')
-      .resolves(true)
+      .stub(mail, "sendAvtalegiroNotification")
+      .resolves(true);
 
     shipmentStub = sinon
-      .stub(DAO.avtalegiroagreements, 'addShipment')
-      .resolves(42)
+      .stub(DAO.avtalegiroagreements, "addShipment")
+      .resolves(42);
 
-    agreementsStub = sinon
-      .stub(DAO.avtalegiroagreements, 'getByPaymentDate')
-    
-    agreementsStub.withArgs(9).resolves([])
-    agreementsStub.withArgs(10).resolves(mockAgreements)
-    agreementsStub.withArgs(28).resolves(mockAgreements)
-    agreementsStub.withArgs(31).resolves([])
+    agreementsStub = sinon.stub(DAO.avtalegiroagreements, "getByPaymentDate");
 
-    loggingStub = sinon
-      .stub(DAO.logging, 'add')
-      .resolves(true)
+    agreementsStub.withArgs(9).resolves([]);
+    agreementsStub.withArgs(10).resolves(mockAgreements);
+    agreementsStub.withArgs(28).resolves(mockAgreements);
+    agreementsStub.withArgs(31).resolves([]);
 
-    sendFileStub = sinon
-      .stub(nets, 'sendFile')
+    loggingStub = sinon.stub(DAO.logging, "add").resolves(true);
 
-    sendMailBackupStub = sinon
-      .stub(mail, 'sendOcrBackup')
+    sendFileStub = sinon.stub(nets, "sendFile");
 
-    const scheduledRoute = require('../routes/scheduled')
-    server = express()
-    server.use('/scheduled', scheduledRoute)
-  })
+    sendMailBackupStub = sinon.stub(mail, "sendOcrBackup");
 
-  beforeEach(function() {
-    sinon.resetHistory()
-  })
+    const scheduledRoute = require("../routes/scheduled");
+    server = express();
+    server.use("/scheduled", scheduledRoute);
+  });
 
-  it('Does nothing with no agreements', async function() {
-    agreementsStub.resolves([])
+  beforeEach(function () {
+    sinon.resetHistory();
+  });
+
+  it("Does nothing with no agreements", async function () {
+    agreementsStub.resolves([]);
 
     const response = await request(server)
-      .post('/scheduled/avtalegiro?date=2021-10-03')
-      .expect(200)
+      .post("/scheduled/avtalegiro?date=2021-10-03")
+      .expect(200);
 
-    expect(agreementsStub.calledOnce).to.be.true
-    expect(sendNotificationStub.called).to.be.false
-    expect(sendFileStub.called).to.be.false
-    expect(loggingStub.calledOnce).to.be.true
-    expect(sendMailBackupStub.calledOnce).to.be.true
-  })
+    expect(agreementsStub.calledOnce).to.be.true;
+    expect(sendNotificationStub.called).to.be.false;
+    expect(sendFileStub.called).to.be.false;
+    expect(loggingStub.calledOnce).to.be.true;
+    expect(sendMailBackupStub.calledOnce).to.be.true;
+  });
 
-  it('Generates claim file when provided a date', async function() {
-    agreementsStub.resolves(mockAgreements)
+  it("Generates claim file when provided a date", async function () {
+    agreementsStub.resolves(mockAgreements);
 
     const response = await request(server)
-      .post('/scheduled/avtalegiro/?date=2021-10-04')
-      .expect(200)
+      .post("/scheduled/avtalegiro/?date=2021-10-04")
+      .expect(200);
 
-    expect(sendNotificationStub.called).to.be.false
-    expect(sendFileStub.calledOnce).to.be.true
-  })
+    expect(sendNotificationStub.called).to.be.false;
+    expect(sendFileStub.calledOnce).to.be.true;
+  });
 
-  it('Notifies claimants when they have asked for it', async function() {
-    agreementsStub.resolves(mockAgreements)
+  it("Notifies claimants when they have asked for it", async function () {
+    agreementsStub.resolves(mockAgreements);
 
     // Used to force mail to be sent (but method is stubbed)
-    let tempEnv = config.env
-    config.env = 'production'
+    let tempEnv = config.env;
+    config.env = "production";
 
     const response = await request(server)
-      .post('/scheduled/avtalegiro/?date=2021-10-04&notify=true')
-      .expect(200)
+      .post("/scheduled/avtalegiro/?date=2021-10-04&notify=true")
+      .expect(200);
 
-    config.env = tempEnv
+    config.env = tempEnv;
 
-    expect(sendNotificationStub.callCount).to.be.equal(2)
-    expect(sendFileStub.calledOnce).to.be.true
-  })
+    expect(sendNotificationStub.callCount).to.be.equal(2);
+    expect(sendFileStub.calledOnce).to.be.true;
+  });
 
-  it('Recognizes when claim date is last day of the month', async function() {
+  it("Recognizes when claim date is last day of the month", async function () {
     const response = await request(server)
-      .post('/scheduled/avtalegiro/?date=2021-10-25&notify=true')
-      .expect(200)
+      .post("/scheduled/avtalegiro/?date=2021-10-25&notify=true")
+      .expect(200);
 
-    sinon.assert.calledWithExactly(agreementsStub, 0)
-    sinon.assert.calledWithExactly(shipmentStub, mockAgreements.length)
-  })
+    sinon.assert.calledWithExactly(agreementsStub, 0);
+    sinon.assert.calledWithExactly(shipmentStub, mockAgreements.length);
+  });
 
-  it('Includes the 28th when the 28th is last day of month', async function() {
+  it("Includes the 28th when the 28th is last day of month", async function () {
     const respnse = await request(server)
-      .post('/scheduled/avtalegiro/?date=2022-02-22&notify=true')
-      .set('Authorization', 'Bearer abc')
-      .expect(200)
+      .post("/scheduled/avtalegiro/?date=2022-02-22&notify=true")
+      .set("Authorization", "Bearer abc")
+      .expect(200);
 
-    sinon.assert.calledWithExactly(agreementsStub, 0)
-    sinon.assert.calledWithExactly(agreementsStub, 28)
+    sinon.assert.calledWithExactly(agreementsStub, 0);
+    sinon.assert.calledWithExactly(agreementsStub, 28);
     // Should have 2x mock agreements, one x for the last of the month, one x for the 28th
-    sinon.assert.calledWithExactly(shipmentStub, mockAgreements.length*2)
-  })
+    sinon.assert.calledWithExactly(shipmentStub, mockAgreements.length * 2);
+  });
 
   after(function () {
-    sinon.restore()
-  })
-})
+    sinon.restore();
+  });
+});
