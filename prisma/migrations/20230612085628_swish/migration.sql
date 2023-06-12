6b2fd3bf7eec9c0d512c9eee34e5b415549794a8
Adam Alfredsson <alfredsson.adam@gmail.com>
2023-06-12 09:56:38 +0100

    Update migration

--- prisma/migrations/20230612085628_swish/migration.sql
+++ prisma/migrations/20230612085628_swish/migration.sql
@@ -5,10 +5,11 @@ CREATE TABLE `SwishOrder` (
     `instructionUUID` VARCHAR(36) NOT NULL,
     `donorID` INTEGER NOT NULL,
     `donationID` INTEGER NULL,
-    `status` ENUM('PAID', 'DECLINED', 'ERROR', 'CANCELLED') NULL,
+    `status` VARCHAR(191) NULL,
     `reference` VARCHAR(32) NOT NULL,
     `registered` DATETIME(0) NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
 
+    UNIQUE INDEX `SwishOrder_KID_key`(`KID`),
     UNIQUE INDEX `SwishOrder_instructionUUID_key`(`instructionUUID`),
     PRIMARY KEY (`ID`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
