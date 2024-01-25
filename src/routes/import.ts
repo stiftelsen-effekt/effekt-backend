7fd781da3cc571059f614a3ea828552ffd0f5b6d
Håkon Harnes <account@harnes.me>
2024-01-25 15:44:25 +0100

    Re-enable admin check for swedish import endpoint

--- src/routes/import.ts
+++ src/routes/import.ts
@@ -4,30 +4,27 @@ import { isAdmin } from "../custom_modules/authorization/authMiddleware";
 
 export const importRouter = Router();
 
-importRouter.post(
-  "/donations/se",
-  /*isAdmin,*/ async (req, res, next) => {
-    const report = req.files.report;
+importRouter.post("/donations/se", isAdmin, async (req, res, next) => {
+  const report = req.files.report;
 
-    if (!report) {
-      return res.status(400).json({
-        status: 400,
-        message: "Missing report",
-      });
-    }
-
-    if (Array.isArray(report)) {
-      return res.status(400).json({
-        status: 400,
-        message: "Multiple reports not supported",
-      });
-    }
-
-    const result = await importSwedishDonationsReport(report.data);
-
-    res.json({
-      status: 200,
-      content: result,
+  if (!report) {
+    return res.status(400).json({
+      status: 400,
+      message: "Missing report",
     });
-  },
-);
+  }
+
+  if (Array.isArray(report)) {
+    return res.status(400).json({
+      status: 400,
+      message: "Multiple reports not supported",
+    });
+  }
+
+  const result = await importSwedishDonationsReport(report.data);
+
+  res.json({
+    status: 200,
+    content: result,
+  });
+});
