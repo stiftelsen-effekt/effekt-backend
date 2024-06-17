837283b4412cdb0a939928383c48a698785cdc53
Håkon Harnes <account@harnes.me>
2024-06-17 15:04:15 +0200

    Use node encodings when detecting type

--- src/routes/reports/autogiro.ts
+++ src/routes/reports/autogiro.ts
@@ -4,7 +4,7 @@ import chardet from "chardet";
 
 module.exports = async (req, res, next) => {
   const encoding = chardet.detect(req.files.report.data);
-  var data = req.files.report.data.toString(encoding);
+  var data = req.files.report.data.toString(getNodeEncoding(encoding));
 
   try {
     const result = await processAutogiroInputFile(data);
@@ -56,3 +56,38 @@ module.exports = async (req, res, next) => {
     return next({ ex: ex });
   }
 };
+
+const encodingMap = {
+  "UTF-8": "utf8",
+  "UTF-16 LE": "utf16le",
+  "UTF-16 BE": "utf16be",
+  "UTF-32 LE": "utf32le",
+  "UTF-32 BE": "utf32be",
+  "ISO-2022-JP": "iso-2022-jp",
+  "ISO-2022-KR": "iso-2022-kr",
+  "ISO-2022-CN": "iso-2022-cn",
+  Shift_JIS: "shift_jis",
+  Big5: "big5",
+  "EUC-JP": "euc-jp",
+  "EUC-KR": "euc-kr",
+  GB18030: "gb18030",
+  "ISO-8859-1": "latin1",
+  "ISO-8859-2": "latin2",
+  "ISO-8859-5": "iso-8859-5",
+  "ISO-8859-6": "iso-8859-6",
+  "ISO-8859-7": "iso-8859-7",
+  "ISO-8859-8": "iso-8859-8",
+  "ISO-8859-9": "iso-8859-9",
+  "windows-1250": "win1250",
+  "windows-1251": "win1251",
+  "windows-1252": "win1252",
+  "windows-1253": "win1253",
+  "windows-1254": "win1254",
+  "windows-1255": "win1255",
+  "windows-1256": "win1256",
+  "KOI8-R": "koi8-r",
+};
+
+function getNodeEncoding(chardetEncoding) {
+  return encodingMap[chardetEncoding] || null;
+}
