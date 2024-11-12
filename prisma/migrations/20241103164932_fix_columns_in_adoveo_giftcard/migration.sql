ba48f9d445d01774c54b98e92af36ef24cbec6ec
Håkon Harnes <account@harnes.me>
2024-11-12 11:36:44 +0100

    Removes unneeded dropping of foreign key

--- prisma/migrations/20241103164932_fix_columns_in_adoveo_giftcard/migration.sql
+++ prisma/migrations/20241103164932_fix_columns_in_adoveo_giftcard/migration.sql
@@ -9,8 +9,6 @@
   - Added the required column `Name` to the `Adoveo_giftcard` table without a default value. This is not possible if the table is not empty.
 
 */
--- DropForeignKey
-ALTER TABLE `Adoveo_giftcard` DROP FOREIGN KEY `FK_Adoveo_giftcards_donorid`;
 
 -- DropIndex
 DROP INDEX `Hash_UNIQUE` ON `Adoveo_giftcard`;
